c466a47fb25f8ad85632055b08f55a983715ee97
Kristi Hwang <kristi.y.l.h@gmail.com>
2024-10-15 16:46:15 -0700

    install tailwind and save fonts in config

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,21 +1,21 @@
-import Navbar from "./Navbar"
-import rinja from "../assets/mt-rinja.jpg"
-import './Landing.css'
+import Navbar from "./Navbar";
+import rinja from "../assets/mt-rinja.jpg";
+import "./Landing.css";
 
+export default function Landing() {
+  return (
+    <div className="body" style={{ backgroundImage: `url(${rinja})` }}>
+      <Navbar />
+      <div className="img-blurb">
+        <h1 className="libre-baskerville-regular">
+          UNEARTH UNPARALLELED LANDSCAPES OF LOMBOK IN ITS IDYLLIC AND NATURAL
+          SERENITY
+        </h1>
 
-export default function Landing(){
-    return(
-      
-        <div className="body" style={{ backgroundImage: `url(${rinja})`}}>
-                <Navbar />  
-                <div className="img-blurb">
-                    <h1 className="libre-baskerville-regular">
-                        UNEARTH UNPARALLELED LANDSCAPES OF LOMBOK IN ITS IDYLLIC AND NATURAL SERENITY
-                    </h1>
-                
-                    <h2 className="libre-baskerville-regular-italic">Your private villa awaits. </h2> 
-                </div>
-          
-        </div>
-    )
+        <h2 className="libre-baskerville-regular-italic">
+          Your private villa awaits.{" "}
+        </h2>
+      </div>
+    </div>
+  );
 }
