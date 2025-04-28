0d9d4859fc22e67fd7c2d7eca4216b35a0f203cb
Kristi Hwang <kristi.y.l.h@gmail.com>
2025-04-27 23:30:06 -0700

    refactor(App): clean up routes with SharedLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,37 @@
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Landing from "./components/modules/Landing";
 import Villa from "./components/pages/Villa";
 import Book from "./components/pages/Book";
 import Local from "./components/pages/Local";
 import Story from "./components/pages/Story";
-import {
-  createBrowserRouter,
-  BrowserRouter,
-  // Routes,
-  // Route,
-} from "react-router-dom";
-import "./index.css";
 import Header from "./components/ui/Header";
 import Footer from "./components/ui/Footer";
+import "./index.css";
+
+function SharedLayout() {
+  return (
+    <div className="w-screen overflow-x-hidden scroll-smooth bg-primary text-white">
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <SharedLayout />,
+    children: [
+      { index: true, element: <Landing /> },
+      { path: "villa", element: <Villa /> },
+      { path: "book", element: <Book /> },
+      { path: "local", element: <Local /> },
+      { path: "story", element: <Story /> },
+    ],
+  },
+]);
 
 export default function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <div>Hello world!</div>,
-    },
-  ]);
-
-  return (
-    <BrowserRouter router={router}>
-      <div className="w-screen overflow-x-hidden scroll-smooth bg-primary text-white">
-        <Header />
-        <Landing />
-        <div id="villa">
-          <Villa />
-        </div>
-        <div id="book">
-          <Book />
-        </div>
-        <div id="local">
-          <Local />
-        </div>
-        <div id="story">
-          <Story />
-        </div>
-        <Footer />
-      </div>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 }
