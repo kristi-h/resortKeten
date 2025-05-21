96ed3a2751d2474ebd013e2c3b65f30837e69742
Kristi Hwang <kristi.y.l.h@gmail.com>
2025-05-20 23:59:32 -0700

    refactor(Landing): add button fo Book

--- src/components/modules/Landing.jsx
+++ src/components/modules/Landing.jsx
@@ -3,7 +3,7 @@ import hero from "../../assets/landing/seagreen.mp4";
 
 export default function Landing() {
   return (
-    <div className="w-screen h-screen bg-cover bg-center flex flex-col justify-center items-center">
+    <div className="w-screen h-screen bg-cover bg-center flex items-center justify-center relative overflow-hidden">
       <video
         autoPlay
         loop
@@ -15,11 +15,17 @@ export default function Landing() {
         Your browser does not support the video tag.
       </video>
 
-      <div className="text-center space-y-8 text-orange-200 bg-black bg-opacity-50 p-8 rounded-lg">
+      <div className="relative z-10 text-center space-y-6 text-orange-200 bg-black bg-opacity-50 p-8 rounded-lg">
         <Link to="/local">
-          <h1 className="absolute bottom-[20%] left-1/2 transform -translate-x-1/2 text-4xl font-bold sm:text-4xl md:text-6xl lg:text-6xl text-black mb-4 font-saol drop-shadow-lg hover:text-white transition-colors duration-300 cursor-pointer">
+          <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold font-saol text-black hover:text-white transition duration-300 cursor-pointer">
             Explore Lombok
           </h1>
+        </Link>
+
+        <Link to="/book">
+          <button className="mt-2 px-6 py-3 bg-yellow-800 text-white font-gara text-lg rounded-md hover:bg-yellow-700 transition">
+            Book Your Stay
+          </button>
         </Link>
       </div>
     </div>
