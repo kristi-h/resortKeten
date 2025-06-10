aad179ace1272c83e7144b6ab0592d84f5a1cbd4
Kristi Hwang <kristi.y.l.h@gmail.com>
2025-06-10 14:20:54 -0700

    refactor(Find): add coords for villa and edit marker to that loc, center map on lombok coords

--- src/components/pages/Find.jsx
+++ src/components/pages/Find.jsx
@@ -2,9 +2,21 @@ import LocalMap from "../modules/LocalMap";
 
 export default function Find() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-primary to-sky-100 px-4 py-8">
-      <div className="w-full max-w-4xl h-[650px] rounded-2xl overflow-hidden border border-gray-300 shadow-xl">
-        <LocalMap />
+    <div className="px-4 py-12 bg-primary min-h-screen">
+      <div className="max-w-3xl mx-auto text-center mb-10">
+        <h2 className="text-6xl font-bold text-yellow-700 font-saol mb-3 tracking-tight leading-tight mx-auto">
+          Discover Local Gems
+        </h2>
+        <p className="text-lg text-yellow-600 font-saol leading-relaxed">
+          Explore restaurants, beaches, cultural sites, and unforgettable
+          experiences near our villa.
+        </p>
+      </div>
+
+      <div className="flex items-center justify-center">
+        <div className="w-full max-w-5xl h-[700px] rounded-2xl overflow-hidden border border-gray-300 shadow-2xl">
+          <LocalMap />
+        </div>
       </div>
     </div>
   );
